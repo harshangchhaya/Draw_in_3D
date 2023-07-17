8ea6496788218a9a2f5b030b013e4ee8632c54c1
Harshang Chhaya <harshangchhaya@Harshangs-MacBook-Air.local>
2023-07-17 13:41:49 -0400

    Controls and Interactions added

--- Draw in 3D/Draw in 3D/ViewController.swift
+++ Draw in 3D/Draw in 3D/ViewController.swift
@@ -12,9 +12,21 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var currentSpherePosition: SCNVector3!
+    var currentSphereColor: UIColor!
     var sphereNode: SCNNode!
     var colorPicker: UIColorWell!
     let placeButton = UIButton()
+    let radiusLabel = UILabel()
+    var radiusValue: Int = 50
+    var rawRadiusScale: Double = 50 {
+        didSet {
+            // Ensure value stays within the range of 1 to 100
+            rawRadiusScale = max(1, min(91, rawRadiusScale))
+            radiusValue = Int(ceil(100 - rawRadiusScale + 1))
+            updateRadiusLabel(radiusValue)
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -25,8 +37,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         self.configureButton()
         self.configureColorPicker()
-        let sphere = SCNSphere(radius: 0.1)
+        self.configureRadiusLabel()
+        let sphere = SCNSphere(radius: 0.02)
         sphereNode = SCNNode(geometry: sphere)
+        sphereNode.opacity = 0.5
+        
+        
         sceneView.scene.rootNode.addChildNode(sphereNode)
         
         // Show statistics such as fps and timing information
@@ -59,9 +75,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
         
     }
+    
+    @objc func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+        if gesture.state == .changed {
+            let pinchScale = Double(gesture.scale)
+            let sensitivity: Double = 10.0
+            let delta: Double = sensitivity * (1-pinchScale)
+
+            // Update the value based on the pinch gesture
+            rawRadiusScale += delta
+            gesture.scale = 1.0
+        }
+    }
+    
+    @objc func placeSphereAtLocation() {
+        let sphere = SCNSphere(radius: 0.025)
+        let placedSphere = SCNNode(geometry: sphere)
+        placedSphere.position = currentSpherePosition
+        placedSphere.geometry?.firstMaterial?.diffuse.contents = currentSphereColor
+        placedSphere.geometry?.setValue( (Float(radiusValue)/10)*0.025, forKey: "radius")
+        
+        sceneView.scene.rootNode.addChildNode(placedSphere)
+    }
+    
+    @objc func changeColor() {
+        currentSphereColor = colorPicker.selectedColor
+    }
+    
+    func configureRadiusLabel() {
+        radiusLabel.translatesAutoresizingMaskIntoConstraints = false
+        radiusLabel.textAlignment = .center
+        view.addSubview(radiusLabel)
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
+        view.addGestureRecognizer(pinchGesture)
+        
+        NSLayoutConstraint.activate([
+                radiusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                radiusLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32)
+            ])
+    }
+    
     func configureColorPicker() {
         colorPicker = UIColorWell()
         view.addSubview(colorPicker)
+        colorPicker.addTarget(self, action: #selector(changeColor), for: .valueChanged)
         
         colorPicker.supportsAlpha = false
         colorPicker.title = "Color"
@@ -76,6 +133,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         view.addSubview(placeButton)
         placeButton.configuration = .gray()
         placeButton.configuration?.title = "Place"
+        placeButton.addTarget(self, action: #selector(placeSphereAtLocation), for: .touchUpInside)
         
         placeButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -99,8 +157,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return circleNode
         }
     
+    func updateRadiusLabel(_ value: Int) {
+        radiusLabel.text = "zoom: \(value)"
+    }
+    
+    
     func updateSpherePosition(_ position: SCNVector3){
         sphereNode.position = position
+        print(Float(radiusValue/10)*0.025)
+        //sphereNode.geometry.setValue(
+        sphereNode.geometry?.firstMaterial?.diffuse.contents = currentSphereColor
+        sphereNode.geometry?.setValue( (Float(radiusValue)/10)*0.025, forKey: "radius")
+        //sphereNode.geometry?.setValue(<#T##value: Any?##Any?#>, forKey: <#T##String#>)
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
@@ -122,8 +191,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let ct = frame.camera.transform
             let transformedPoint = simd_mul(ct, ref)
             let transformedPosition = SCNVector3(x: transformedPoint.x, y: transformedPoint.y, z: transformedPoint.z)
+            self.currentSpherePosition = transformedPosition
         
-        self.updateSpherePosition(SCNVector3(x: transformedPoint.x, y: transformedPoint.y, z: transformedPoint.z))
+            self.updateSpherePosition(SCNVector3(x: transformedPoint.x, y: transformedPoint.y, z: transformedPoint.z))
             
             //let cameraPosition = SCNVector3(cameraTransform.m41, cameraTransform.m42, cameraTransform.m43-distance)
         
